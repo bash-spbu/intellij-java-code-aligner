281aa65156bee736591379578df2d532b6cd5bb0
GitHub Action <action@github.com>
2021-10-17 15:22:13 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bashspbu/intellijjavacodealigner/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bashspbu.intellijjavacodealigner.services
+
+import com.github.bashspbu.intellijjavacodealigner.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/bashspbu/intellijjavacodealigner/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bashspbu/intellijjavacodealigner/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bashspbu.intellijjavacodealigner.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bashspbu.intellijjavacodealigner.MyBundle
 
 class MyProjectService(project: Project) {
 
